1aef2c76264ce1c4206de9c9be6b6a812e03f930
mayanpathak <ranupathak1986@gmail.com>
2025-06-29 21:04:38 +0530

    added

--- Backend/server.js
+++ Backend/server.js
@@ -35,7 +35,7 @@ const app = express();
 // Middleware
 app.use(cors({
   origin: [
-    process.env.FRONTEND_URL || 'http://localhost:3000',
+    process.env.FRONTEND_URL || 'http://localhost:3000', 'http://localhost:3001', 
     'https://github.com/mayanpathak/digitalyze' || 'https://github.com/mayanpathak/digitalyze'// Allow both ports for development
   ],
   credentials: true
